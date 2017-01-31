fd93663647667cb7582de1e388c74ee79d10cb9e
David <46406163y@iespoblenou.org>
2017-01-31 17:34:08 +0100

    Creacio CustomViewPager i bici_row

--- app/src/main/java/com/example/a46406163y/bicingmaps/MainActivityFragment.java
+++ app/src/main/java/com/example/a46406163y/bicingmaps/MainActivityFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.app.Application;
+import android.widget.TextView;
+import android.widget.ListView;
 
 /**
  * A placeholder fragment containing a simple view.
